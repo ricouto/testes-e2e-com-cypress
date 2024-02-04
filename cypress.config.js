dbbaed090107b8f98481d8421a564199364f7ca5
ricouto <ricardocaputo@gmail.com>
2024-02-04 14:09:14 -0300

    feat(aula29): integracao com cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    projectId: 'u1fgwa',
     baseUrl: 'https://notes-serverless-app.com',
     chromeWebSecurity: false,
     defaultCommandTimeout: 15000,
